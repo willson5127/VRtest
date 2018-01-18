a2fe2efaaf3789ebe878355a65e63c05d8ca0f7d
willson-PC\willson <willson5127@hotmail.com>
2018-01-18 23:25:22 +0800

    backtest

---
+++ Assets/123/Scripts/JunkRatController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JunkRatController : MonoBehaviour
+{
+
+    [SerializeField] float WalkVelocity;
+    [SerializeField] float AirControl;
+    [SerializeField] float JumpSpeed;
+
+    Rigidbody RB;
+    CharacterController CC;
+    Camera camera;
+    [SerializeField]bool IsGrounded;
+
+    // Use this for initialization
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+        camera = GetComponentInChildren<Camera>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HeadTurn();
+    }
+    void FixedUpdate()
+    {
+        Walk();
+        Jump();
+    }
+    void Walk()
+    {
+        if (IsGrounded)
+        {
+            RB.velocity = transform.forward * Input.GetAxis("Vertical") * WalkVelocity + transform.right * Input.GetAxis("Horizontal") * WalkVelocity + new Vector3(0, RB.velocity.y, 0);
+        }
+        else
+        {
+            RB.velocity = transform.forward * Input.GetAxis("Vertical") * AirControl + transform.right * Input.GetAxis("Horizontal") * AirControl + new Vector3(0, RB.velocity.y, 0);
+        }
+    }
+    void HeadTurn()
+    {
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0));
+        camera.transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"),0 , 0));
+    }
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && IsGrounded)
+        {
+            RB.velocity += transform.up * JumpSpeed;
+        }
+    }
+
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            print(other.gameObject.name);
+            IsGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            IsGrounded = false;
+        }
+    }
+}
+
---
+++ Assets/123/Scripts/Mine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour {
+
+    [SerializeField]GameObject Effect;
+    [SerializeField] LayerMask layerMask;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Boom()
+	{
+        Collider[] Hit = Physics.OverlapSphere(transform.position, 5, layerMask);
+		if(Hit.Length>0)
+		{
+			for (int i = 0; i < Hit.Length; i++)
+			{
+                    print(Hit[i].name);
+                Hit[i].GetComponent<Rigidbody>().velocity += transform.up * 10000;
+            }
+		}
+        Instantiate(Effect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
